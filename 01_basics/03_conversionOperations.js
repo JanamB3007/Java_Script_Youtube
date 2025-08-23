2dd8f374a597e39cb86ff1b8e8792a32797dd08f
Janam Bhalala <128905879+JanamB3007@users.noreply.github.com>
2025-08-23 12:28:20 +0000

    modified dev container and add file number 03

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,20 @@
+// Scenario 1
+
+// let score = 33;
+
+// console.log(typeof score);
+// output ==> number
+
+// console.log(typeof (score));
+// output ==> number
+
+
+// scenario 2
+
+let score = "33";
+
+console.log(typeof score);
+// output ==> number
+console.log(typeof (score));
+// output ==> number
+
